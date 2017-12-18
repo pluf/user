29bec28cd70232d65b985a312da4d00d57352176
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2017-12-18 12:30:56 +0330

    Add QCA links to readme

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Pluf/User
 
+
+### Develop
+
+[![Build Status](https://travis-ci.org/pluf/user.svg?branch=develop)](https://travis-ci.org/pluf/user)
+[![codecov](https://codecov.io/gh/pluf/user/branch/develop/graph/badge.svg)](https://codecov.io/gh/pluf/user)
+[![Coverage Status](https://coveralls.io/repos/github/pluf/user/badge.svg?branch=develop)](https://coveralls.io/github/pluf/user?branch=develop)
+
+### Master
+
+[![Build Status](https://travis-ci.org/pluf/user.svg?branch=master)](https://travis-ci.org/pluf/user)
+[![codecov](https://codecov.io/gh/pluf/user/branch/master/graph/badge.svg)](https://codecov.io/gh/pluf/user)
+[![Coverage Status](https://coveralls.io/repos/github/pluf/user/badge.svg?branch=master)](https://coveralls.io/github/pluf/user?branch=master)
+[![Maintainability](https://api.codeclimate.com/v1/badges/9e1457dbf2f0bcc8b953/maintainability)](https://codeclimate.com/github/pluf/user/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/9e1457dbf2f0bcc8b953/test_coverage)](https://codeclimate.com/github/pluf/user/test_coverage)
+
+
 این پروژه یک ماژول برای سرورهای pluf است که امکانات مدیریت پروفایل‌ها و حساب‌های کاربری را فراهم می‌کند.
 
 این ماژول به ماژول‌های زیر وابستگی دارد:
--- tests/User_Monitor/BasicTest.php
+++ tests/User_Monitor/BasicTest.php
@@ -32,48 +32,7 @@ class User_Monitor_BasicsTest extends TestCase
      */
     public static function createDataBase()
     {
-        Pluf::start(array(
-            'general_domain' => 'localhost',
-            'general_admin_email' => array(
-                'root@localhost'
-            ),
-            'general_from_email' => 'test@localhost',
-            'middleware_classes' => array(
-                'Pluf_Middleware_Session'
-            ),
-            'debug' => true,
-            'test_unit' => true,
-            'languages' => array(
-                'fa',
-                'en'
-            ),
-            'tmp_folder' => dirname(__FILE__) . '/../tmp',
-            'template_folders' => array(
-                dirname(__FILE__) . '/../templates'
-            ),
-            'upload_path' => dirname(__FILE__) . '/../tmp',
-            'template_tags' => array(),
-            'time_zone' => 'Asia/Tehran',
-            'encoding' => 'UTF-8',
-            
-            'secret_key' => '5a8d7e0f2aad8bdab8f6eef725412850',
-            'user_signup_active' => true,
-            'user_avatra_max_size' => 2097152,
-            'auth_backends' => array(
-                'Pluf_Auth_ModelBackend'
-            ),
-            'pluf_use_rowpermission' => true,
-            'db_engine' => 'MySQL',
-            'db_version' => '5.5.33',
-            'db_login' => 'root',
-            'db_password' => '',
-            'db_server' => 'localhost',
-            'db_database' => 'test',
-            'db_table_prefix' => '_tm_',
-            
-            'mail_backend' => 'mail',
-            'user_avatar_default' => dirname(__FILE__) . '/../conf/avatar.svg'
-        ));
+        Pluf::start();
         $db = Pluf::db();
         $schema = Pluf::factory('Pluf_DB_Schema', $db);
         $models = array(
@@ -97,7 +56,7 @@ class User_Monitor_BasicsTest extends TestCase
             }
         }
         
-        $user = new Pluf_User();
+        $user = new User();
         $user->login = 'test';
         $user->first_name = 'test';
         $user->last_name = 'test';
@@ -158,7 +117,7 @@ class User_Monitor_BasicsTest extends TestCase
         ));
         
         // Change detail
-        $user = new Pluf_User();
+        $user = new User();
         $user = $user->getUser('test');
         
         // Login
